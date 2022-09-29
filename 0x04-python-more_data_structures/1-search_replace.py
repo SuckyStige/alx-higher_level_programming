316bd2afec73b21e73cbbc6d42f8a0d233e2f735
Sucky Stige <stigehub@gmail.com>
2022-09-29 08:28:51 -0700

    Search and Replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-def search_replace(mylist, search, replace):
-    new_1 = []
-
-    for y in mylist:
-        if y == search:
-            new_1.append(replace)
-        else:
-            new_1.append(y)
-            return 
+def search_replace(my_list, search, replace):
+    return[x if x != search else replace for x in my_list] 
