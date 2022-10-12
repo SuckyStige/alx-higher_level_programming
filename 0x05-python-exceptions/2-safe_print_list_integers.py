3ff4cf81512e8de5a060ca9f929ab3af159dba49
Sucky Stige <stigehub@gmail.com>
2022-10-12 14:28:36 -0700

    function that prints an integer.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-
-
 def safe_print_list_integers(my_list=[], x=0):
-    print = 0
-
+    c = 0
     for i in range(x):
         try:
-            if type(my_list[i]) is int and printed != x:
-                print('{:d}'.format(my_list[i], end='')
-                printed += 1
-         except TypeError:
-         continue
-
-    print()
-
-    return printed
+            print("{:d}".format(my_list[i]), end="")
+            c += 1
+        except (ValueError, TypeError):
+            continue
+        print()
+        return 
